22053088d9a1bcb2bf208d456bcf1a68832a364d
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-10-09 12:41:19 +0200

    caching implementation

--- src/offapi/offapi.module.ts
+++ src/offapi/offapi.module.ts
@@ -1,11 +1,15 @@
 import { HttpModule } from '@nestjs/axios';
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { OffapiService } from './offapi.service';
 import { OffapiController } from './offapi.controller';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register({ ttl: 60, max: 5 })],
   controllers: [OffapiController],
-  providers: [OffapiService],
+  providers: [
+    OffapiService,
+    { provide: APP_INTERCEPTOR, useClass: CacheInterceptor },
+  ],
 })
 export class OffapiModule {}
--- src/offapi/offapi.service.ts
+++ src/offapi/offapi.service.ts
@@ -1,14 +1,18 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, CACHE_MANAGER, Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class OffapiService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
   async findOne(barcode: string): Promise<any> {
     const result = await this.httpService.axiosRef.get(
       `https://world.openfoodfacts.org/api/v2/product/${barcode}`,
     );
-    console.log('RESULT', result.data);
+    console.log('Cache', await this.cacheManager.get('key'));
     return result.data;
   }
 }
