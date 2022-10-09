c56506e32f374f25890f8ba6d36291b062af6d0d
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-10-09 16:22:06 +0200

    safer axios request

--- src/offapi/offapi.service.ts
+++ src/offapi/offapi.service.ts
@@ -6,10 +6,14 @@ export class OffapiService {
   constructor(private readonly httpService: HttpService) {}
 
   async findOne(barcode: string): Promise<any> {
-    const result = await this.httpService.axiosRef.get(
-      `https://world.openfoodfacts.org/api/v2/product/${barcode}`,
-    );
-    console.log('getting here because response is not yet cached');
+    const baseURL = 'https://world.openfoodfacts.org/api/v2/product/';
+    const result = await this.httpService.axiosRef.request({
+      url: barcode,
+      baseURL: baseURL,
+      headers: {
+        'User-Agent': 'Crud Assignment - Version 1.0',
+      },
+    });
     return result.data;
   }
 }
