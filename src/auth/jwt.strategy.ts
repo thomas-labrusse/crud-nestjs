147007ab46d28f6f6096183eddfddfdf4a532f95
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-10-09 12:40:49 +0200

    improved import lisibility

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,6 +1,6 @@
+import { Injectable } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
 import { jwtConstants } from './constants';
 
 @Injectable()
@@ -16,7 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   //   Passport verifies the JWT's signature and decodes the JSON, then invokes the validate() method passing the decoded JSON as its single parameter.
   async validate(payload: any) {
-    console.log(`got here with payload : ${payload}`);
     return { id: payload.sub, email: payload.email };
   }
 }
