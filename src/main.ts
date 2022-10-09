868ec45573a007f6f8c896e5485c3be9b027d8fa
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-10-09 12:39:48 +0200

    remove unused import

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
