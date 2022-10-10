1c0ed3537bab60bbe684a286f367d0ec4e855776
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-10-10 13:39:33 +0200

    test setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OffapiModule } from './offapi/offapi.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './users/user.entity';
 
 @Module({
@@ -12,13 +13,23 @@ import { User } from './users/user.entity';
     UsersModule,
     AuthModule,
     OffapiModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
     // TypeOrmModule.forRoot(): sharing db connection with all modules
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User],
-      // synchronize option: only for development purposes. NEVER RUN IN PRODUCTION, CAN DELETE COLUMNS AND DATA, WRITE MIGRATIONS INSTEAD
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          // switching db between dev and test
+          database: config.get<string>('DB_NAME'),
+          entities: [User],
+          // synchronize option: only for development purposes. NEVER RUN IN PRODUCTION, CAN DELETE COLUMNS AND DATA, WRITE MIGRATIONS INSTEAD
+          synchronize: true,
+        };
+      },
     }),
   ],
   controllers: [AppController],
