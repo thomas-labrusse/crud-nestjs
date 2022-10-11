31fd921d531e104517f2b3da3a8427fc289e4e1a
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-10-11 12:28:54 +0200

    welcome route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  hello() {
+    return 'Welcome to the Open Food Fact CRUD, you will find the documentation here : https://github.com/thomas-labrusse/crud-nestjs#readme';
+  }
+}
